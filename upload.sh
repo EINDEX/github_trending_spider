c3353734beb164e34bce719f932560902aaad9ad
EINDEX <snowstarlbk@gmail.com>
2017-08-22 16:20:23 +0800

    travis push

--- upload.sh
+++ upload.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 git remote rm origin
-git remote add origin https://eindex:$GITHUB_API_KEY@github.com/eindex/github_trending_spider.git
+git remote add origin
 git add -f .
 git commit -m `date "+%F"`
-git push -fq origin master > /dev/null
+git -c user.name='travis' -c user.email='travis' commit -m init
+git push -f -q https://eindex:$GITHUB_API_KEY@github.com/eindex/github_trending_spider.git master &2>/dev/null
 echo 'Done'
