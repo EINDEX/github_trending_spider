9f4ada5ac33d23aa15968fbff2491638de9d628d
EINDEX <snowstarlbk@gmail.com>
2017-08-22 15:20:17 +0800

    travis push

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 git remote rm origin
 git remote add origin https://eindex:$GITHUB_API_KEY@github.com/eindex/github_trending_spider.git
+git branch --set-upstream master origin/master
 git add .
 git commit -m `date "+%F"`
 git push -u origin master
